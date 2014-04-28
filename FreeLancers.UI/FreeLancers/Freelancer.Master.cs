90dcb12a8f6950b5a6585cf2ad1632409f5dc742
sherif-adel <sherif.adel90@gmail.com>
2014-04-28 15:21:36 +0200

    -Modifying Folder Structure in UI Project -Adding JQuery Reference

---
+++ FreeLancers.UI/FreeLancers/Freelancer.Master.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FreeLancers.UI.FreeLancers
+{
+	public partial class Freelancer : System.Web.UI.MasterPage
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
