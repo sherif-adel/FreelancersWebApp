f04bd08968e27e34f15dc71dbd3b1fd5c332cd93
sherif-adel <sherif.adel90@gmail.com>
2014-05-05 17:30:49 +0200

    Refactoring DAL and BLL -Removing Static classes and functions, substituting it with abstraction

--- Freelancers.BLL/MainCategoryManager.cs
+++ Freelancers.BLL/MainCategoryManager.cs
@@ -6,70 +6,85 @@ namespace Freelancers.BLL
     using FreeLancers.Models;
     using FreeLancers.DAL;
     
-    public partial class MainCategoryManager
+    public partial class MainCategoryManager : ManagersBase<MainCategory>
     {
+    
+        #region Properties
+    
+    	private MainCategoryDataService _maincategoryDataService;
+
+        #endregion
+
+        #region Constructor
+        public MainCategoryManager(FreeLancersEntities entities):base(entities)
+    	{
+    		_maincategoryDataService = new MainCategoryDataService(entities);
+        }
+
+        #endregion
+
         #region Methods
     
     	/// <summary>
     	/// Gets the specified entity by ID.
     	/// </summary>
     	/// <param name="id">The maincategory ID.</param>
-    	public static MainCategory GetById(int id)
+    	public override MainCategory GetById(int id)
     	{
-    		return MainCategoryDataService.GetById(id);
+    		return _maincategoryDataService.GetById(id);
     	}
     
     	/// <summary>
     	/// Gets All.
     	/// </summary>
-    	public static List<MainCategory> GetAll()
+    	public override List<MainCategory> GetAll()
     	{
-    		return MainCategoryDataService.GetAll();
+    		return _maincategoryDataService.GetAll();
     	}
     
     	/// <summary>
     	/// Adds the specified entity.
     	/// </summary>
     	/// <param name="maincategory">The maincategory entity.</param>
-    	public static void Add(MainCategory maincategory)
+    	public override void Add(MainCategory maincategory)
     	{
-    		MainCategoryDataService.Add(maincategory);
+    		_maincategoryDataService.Add(maincategory);
     	}
     
     	/// <summary>
     	/// Deletes the specified entity.
     	/// </summary>
     	/// <param name="maincategory">The maincategory entity.</param>
-    	public static void Delete(MainCategory maincategory)
+    	public override void Delete(MainCategory maincategory)
     	{
-    		MainCategoryDataService.Delete(maincategory);
+    		_maincategoryDataService.Delete(maincategory);
     	}
     
     	/// <summary>
     	/// Deletes the entity by Id.
     	/// </summary>
     	/// <param name="id">The maincategory Id.</param>
-    	public static void DeleteById(int id)
+    	public override void Delete(int id)
     	{
-    		MainCategoryDataService.DeleteById(id);
+    		_maincategoryDataService.Delete(id);
     	}
     
     	/// <summary>
     	/// Updates the specified entity.
     	/// </summary>
     	/// <param name="maincategory">The maincategory entity.</param>
-    	public static void Update(MainCategory maincategory)
+    	public override void Update(MainCategory maincategory)
     	{
-    		MainCategoryDataService.Update(maincategory);
+    		_maincategoryDataService.Update(maincategory);
     	}
     
     	/// <summary>
     	/// Queries the entity.
     	/// </summary>
     	/// <param name="criteria">Search Criteria.</param>
-    	public static List<MainCategory> Search(System.Linq.Expressions.Expression<Func<MainCategory, bool>> criteria)
+    	public override List<MainCategory> Search(Func<MainCategory, bool> criteria)
     	{
-    		return MainCategoryDataService.Search(criteria);
+    		return _maincategoryDataService.Search(criteria);
     	}
 
         #endregion
